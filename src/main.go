595d1ad67d00152578f7cde01813dcfc56751a62
Javier Pérez <javier.perez@ufirstgroup.ch>
2020-04-16 11:38:09 +0200

    second version: updating fixed, http server up

--- src/main.go
+++ src/main.go
@@ -4,9 +4,9 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"strconv"
 
 	"net/http"
@@ -36,7 +36,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func DbInsertTransaction(sku string, warehouse string, quantity int, description string) {
 	// INSERT transaction
-	// TODO: add dates
 	transactionSql := "INSERT INTO public.transaction (sku, warehouse, quantity, description) VALUES ($1, $2, $3, $4);"
 	_, transactionErr := db.Exec(transactionSql, sku, warehouse, fmt.Sprintf("+%d", quantity), description)
 	if transactionErr != nil {
@@ -50,7 +49,7 @@ func DbUpdateStockAdd(sku string, warehouse string, quantity int) {
 		INSERT INTO stock (sku, warehouse, quantity)
 		VALUES ($1, $2, $3)
 		ON CONFLICT (sku, warehouse)
-		DO UPDATE SET quantity = COALESCE(EXCLUDED.quantity, 0) + $3
+		DO UPDATE SET quantity = COALESCE(stock.quantity, 0) + $3
 		`
 	_, updateErr := db.Exec(sql, sku, warehouse, quantity)
 	if updateErr != nil {
@@ -93,6 +92,7 @@ func DoAddStock(sku string, warehouse string, quantity int, description string) 
 	return newStock
 }
 
+// http://localhost:8000/add/sku/123/warehouse/9/5
 func apiAddStock(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -101,19 +101,21 @@ func apiAddStock(w http.ResponseWriter, r *http.Request) {
 	warehouse := params["warehouse"]
 	stock := params["stock"]
 
-	var Description struct {
-		Text string
-	}
+	//var Description struct {
+	//	Text string
+	//}
 
-	getBodyErr := json.NewDecoder(r.Body).Decode(&Description)
-	if getBodyErr != nil {
-		panic(getBodyErr)
-	}
-	var description = Description.Text
+	//getBodyErr := json.NewDecoder(r.Body).Decode(&Description)
+	//if getBodyErr != nil {
+	//	//panic(getBodyErr)
+	//	log.Fatal(getBodyErr)
+	//}
+
+	//var description = Description.Text
 	quantity, _ := strconv.Atoi(stock)
 
 	// Storage
-	newStock := DoAddStock(sku, warehouse, quantity, description)
+	newStock := DoAddStock(sku, warehouse, quantity, "description")
 
 	_, _ = w.Write([]byte(strconv.Itoa(newStock)))
 }
@@ -133,12 +135,10 @@ func main() {
 
 	db = conn
 
-	DoAddStock("123", "25", 99, "initial stock")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage).Methods("GET")
 
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", homePage).Methods("GET")
-	//
-	//router.HandleFunc("/add/sku/{sku}/warehouse/{warehouse}/{stock}", apiAddStock).Methods("GET")
-	//
-	//log.Fatal(http.ListenAndServe(":8000", router))
+	router.HandleFunc("/add/sku/{sku}/warehouse/{warehouse}/{stock}", apiAddStock).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
