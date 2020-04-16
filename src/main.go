8d08f6ca22d148870f4ae6fd9ceb9c6e5bd94594
Javier Pérez <javier.perez@ufirstgroup.ch>
2020-04-16 12:22:21 +0200

    third version: final rest api calls

--- src/main.go
+++ src/main.go
@@ -4,12 +4,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
-	"strconv"
-
 	"net/http"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -22,17 +22,15 @@ const (
 	dbname   = "appdatabase"
 )
 
-type GetStockResponse struct {
+type AddStockResponse struct {
+	Success   bool   `json:"success"`
+	Message   string `json:"message"`
+	Sku       string `json:"sku"`
 	Warehouse string `json:"warehouse"`
-	Stock     int    `json:"stock"`
+	Quantity  int    `json:"quantity"`
 }
 
 var db *sql.DB
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	//json.NewEncoder(w).Encode(123)
-}
 
 func DbInsertTransaction(sku string, warehouse string, quantity int, description string) {
 	// INSERT transaction
@@ -92,32 +90,57 @@ func DoAddStock(sku string, warehouse string, quantity int, description string) 
 	return newStock
 }
 
-// http://localhost:8000/add/sku/123/warehouse/9/5
+// POST http://localhost:8000/api/{sku}/{warehouse}/{quantity}
 func apiAddStock(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
 	params := mux.Vars(r)
 	sku := params["sku"]
 	warehouse := params["warehouse"]
-	stock := params["stock"]
+	strQuantity := params["quantity"]
+	quantity, _ := strconv.Atoi(strQuantity)
 
-	//var Description struct {
-	//	Text string
-	//}
-
+	// TODO: get text from r.Body
 	//getBodyErr := json.NewDecoder(r.Body).Decode(&Description)
 	//if getBodyErr != nil {
 	//	//panic(getBodyErr)
 	//	log.Fatal(getBodyErr)
 	//}
-
 	//var description = Description.Text
-	quantity, _ := strconv.Atoi(stock)
 
-	// Storage
+	// save data
 	newStock := DoAddStock(sku, warehouse, quantity, "description")
 
-	_, _ = w.Write([]byte(strconv.Itoa(newStock)))
+	// build response
+	response := AddStockResponse{
+		Success:   true,
+		Message:   fmt.Sprintf("Stock added (%d) to %s on %s! New stock = %d", quantity, sku, warehouse, newStock),
+		Sku:       sku,
+		Warehouse: warehouse,
+		Quantity:  newStock,
+	}
+
+	// send response
+	w.Header().Set("Content-Type", "application/json")
+	encodeErr := json.NewEncoder(w).Encode(response)
+	if encodeErr != nil {
+		log.Fatal(encodeErr)
+	}
+}
+
+// DELETE http://localhost:8000/api/{sku}/{warehouse}/{quantity}
+func apiSubStock(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GET http://localhost:8000/api/{sku}
+// GET http://localhost:8000/api/{sku}/{warehouse}
+func apiGetStock(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// GET http://localhost:8000/api/_history/{sku}
+// GET http://localhost:8000/api/_history/{sku}/{warehouse}
+func apiGetHistory(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -136,9 +159,27 @@ func main() {
 	db = conn
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homePage).Methods("GET")
 
-	router.HandleFunc("/add/sku/{sku}/warehouse/{warehouse}/{stock}", apiAddStock).Methods("GET")
+	// TODO: get this from env
+	var baseUri string = "/api"
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	//	GET {sku} --> get stock on all warehouses
+	router.HandleFunc(baseUri+"/{sku}", apiGetStock).Methods("GET")
+	//	GET {sku}/{warehouse} --> get stock on that warehouse
+	router.HandleFunc(baseUri+"/{sku}/{warehouse}", apiGetStock).Methods("GET")
+
+	//	GET _history/{sku} --> get stock history of this product
+	router.HandleFunc(baseUri+"/_history/{sku}", apiGetHistory).Methods("GET")
+	//	GET _history/{sku} --> get stock history of this product and warehouse
+	router.HandleFunc(baseUri+"/_history/{sku}/{warehouse}", apiGetHistory).Methods("GET")
+
+	//	POST {sku}/{warehouse}/{quantity} --> add stock to this product on this warehouse
+	router.HandleFunc(baseUri+"/{sku}/{warehouse}/{quantity}", apiAddStock).Methods("POST")
+
+	//	DELETE {sku}/{warehouse}/{quantity} --> subtract stock to this product on this warehouse
+	router.HandleFunc(baseUri+"/{sku}/{warehouse}/{quantity}", apiSubStock).Methods("DELETE")
+
+	// TODO: get port number from env
+	var portNumber int = 8000
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portNumber), router))
 }
